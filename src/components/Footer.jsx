ad2061b023d7cf6032925387a97d27467105855c
brbsnet <90540181+brbsnet@users.noreply.github.com>
2026-02-04 11:10:32 -0300

    feat: add metricool tracker

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    React.useEffect(() => {
+        // Metricool Tracker
+        function loadScript(a) { var b = document.getElementsByTagName("head")[0], c = document.createElement("script"); c.type = "text/javascript", c.src = "https://tracker.metricool.com/resources/be.js", c.onreadystatechange = a, c.onload = a, b.appendChild(c) }
+        loadScript(function () { beTracker.t({ hash: "50a4ef3cd4e63ff18e7cd32ca7db9738" }) });
+    }, []);
+
     return (
         <footer className="py-20 px-6 bg-[#0f172a] border-t border-white/5 relative overflow-hidden">
             <div className="max-w-7xl mx-auto relative z-10">
